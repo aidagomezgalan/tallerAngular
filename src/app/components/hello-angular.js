82a7209e3d62438d8b81400de1435dd35ae32e8d
Alberto Carretero Arribas <Alberto Carretero Arribas>
2017-04-20 23:09:28 +0200

    subiendo src al master

---
+++ src/app/components/hello-angular.js
@@ -0,0 +1,5 @@
+angular.module('app').component('helloAngular', {
+
+		template: '<h1>Hola Angular 1.6</h1>'
+
+	});
---
+++ src/app/components/my-question.js
@@ -0,0 +1,6 @@
+
+	angular.module('app').component('myQuestion', {
+
+		templateUrl: 'app/components/question-template.html'
+
+	});
