e2a19099d394fb6b4e3fb3db3bf0b05ae482fe84
Alberto Carretero Arribas <Alberto Carretero Arribas>
2017-04-23 23:17:35 +0200

    subiendo rama animalicos

---
+++ animalicos/src/app/components/commons/footer/footer.js
@@ -0,0 +1,14 @@
+(function (angular){
+	angular.module('app').component('footerAnimals',{
+		templateUrl: 'app/components/commons/footer/footer-template.html',
+		controller: footerAnimals,
+		controllerAs: 'footerAnimals'
+
+	});
+
+	function footerAnimals (){
+
+		var vm = this;
+	}
+
+})(angular);
---
+++ animalicos/src/app/components/commons/header/header.js
@@ -0,0 +1,14 @@
+(function (angular){
+	angular.module('app').component('headerAnimals',{
+		templateUrl: 'app/components/commons/header/header-template.html',
+		controller: headerAnimals,
+		controllerAs: 'headerAnimals'
+
+	});
+
+	function headerAnimals (){
+
+		var vm = this;
+	}
+
+})(angular);
